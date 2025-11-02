803b71842c75c8e08ac7c3720130d831d299d3df
thaolinh252525252 <Thaolinh252512@gmail.com>
2025-11-02 18:17:12 +0000

    ok

---
+++ web_app_rfid/app/db_connect.py
@@ -0,0 +1,13 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+def get_db():
+    conn = psycopg2.connect(
+        dbname="iot_db",
+        user="iot",
+        password="2003",
+        host="159.223.63.61",
+        port=5432,
+        cursor_factory=RealDictCursor  # 👈 CHÍNH LÀ DÒNG NÀY
+    )
+    return conn
